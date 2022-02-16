e4586f4dcb2da65c35692ea1330c3ba2946ef86c
jadamcrain <jadamcrain@gmail.com>
2022-02-16 09:55:28 -0800

    trim this down to just the xsd parser

--- tests/xsd_string/expected.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Default, PartialEq, Debug, YaSerialize, YaDeserialize)]
-#[yaserde(prefix = "tns", namespace = "tns: http://example.com")]
-pub struct FooType {
-    #[yaserde(prefix = "tns", rename = "Text")]
-    pub text: String,
-}
-
-impl Validate for FooType {}
