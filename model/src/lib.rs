71bae7c13e9ceac0bc8dbbe0790915e336404384
jadamcrain <jadamcrain@gmail.com>
2022-02-19 10:01:34 -0800

    add serde support to the model

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -1,24 +1,26 @@
 use std::collections::HashMap;
 
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Model {
     pub simple_types: HashMap<String, SimpleType>,
     pub structs: Vec<Struct>,
 }
 
-#[derive(Default, Copy, Clone, Debug)]
+#[derive(Default, Copy, Clone, Debug, Serialize, Deserialize)]
 pub struct StringConstraint {
     pub max_length: Option<usize>,
 }
 
-#[derive(Default, Copy, Clone, Debug)]
+#[derive(Default, Copy, Clone, Debug, Serialize, Deserialize)]
 pub struct NumericConstraint<T> {
     pub min: Option<T>,
     pub max: Option<T>,
 }
 
 // maps to simple types with possible constraints
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 pub enum SimpleType {
     // a single byte encoded as a hex (2 characters e.g. "FF")
     HexByte,
@@ -35,7 +37,7 @@ pub enum SimpleType {
     U64(NumericConstraint<u64>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct StructField {
     pub comment: Option<String>,
     pub name: String,
@@ -43,26 +45,26 @@ pub struct StructField {
     pub info: FieldTypeInfo,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 pub enum ElementType {
     Single,
     Array,
     Option,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 pub enum AttributeType {
     Single,
     Option,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum FieldTypeInfo {
     Attribute(AttributeType),
     Element(ElementType),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Struct {
     pub comment: Option<String>,
     pub name: String,
