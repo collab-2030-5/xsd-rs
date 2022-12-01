06e21be31403868065db9accb1c135b5b624906a
jadamcrain <jadamcrain@gmail.com>
2022-12-01 11:24:22 -0800

    compiling write implementation

--- openadr/src/generated/ei/current_value_type_choice.rs
+++ openadr/src/generated/ei/current_value_type_choice.rs
@@ -1,7 +1,26 @@
-use xml::common::Position;
 use xml::writer::*;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum CurrentValueTypeChoice {
     EiPayloadFloat(crate::ei::PayloadFloatType),
+}
+
+impl CurrentValueTypeChoice {
+    pub(crate) fn write<W>(
+        &self,
+        writer: &mut EventWriter<W>,
+        name: &str,
+    ) -> core::result::Result<(), xml::writer::Error>
+    where
+        W: std::io::Write,
+    {
+        writer.write(events::XmlEvent::start_element(name))?;
+        match self {
+            CurrentValueTypeChoice::EiPayloadFloat(x) => {
+                x.write_with_name(writer, "ei:payloadFloat", false, false)?;
+            }
+        }
+        writer.write(events::XmlEvent::end_element())?;
+        Ok(())
+    }
 }
--- xsd-rs/src/gen/choice.rs
+++ xsd-rs/src/gen/choice.rs
@@ -2,18 +2,20 @@ use heck::ToUpperCamelCase;
 use std::io::Write;
 use xsd_model::resolved::Choice;
 
+use crate::gen::fields::ElementTransforms;
 use crate::gen::{indent, write_comment};
 use crate::{FatalError, RustType};
 
 pub(crate) fn write(w: &mut dyn Write, choice: &Choice) -> Result<(), FatalError> {
     writeln!(w, "use xml::writer::*;")?;
-    writeln!(w, "use xml::common::Position;")?;
     writeln!(w)?;
     write_definition(w, choice)?;
+    writeln!(w)?;
+    write_impl(w, choice)?;
     Ok(())
 }
 
-fn write_definition(w: &mut dyn Write, choice: &Choice) -> Result<(), FatalError> {
+fn write_definition(w: &mut dyn Write, choice: &Choice) -> Result<(), std::io::Error> {
     write_comment(w, &choice.comment)?;
     writeln!(w, "#[derive(Debug, Clone, PartialEq)]")?;
     writeln!(w, "pub enum {} {{", choice.id.name.to_upper_camel_case())?;
@@ -30,5 +32,53 @@ fn write_definition(w: &mut dyn Write, choice: &Choice) -> Result<(), FatalError
         Ok(())
     })?;
     writeln!(w, "}}")?;
+    Ok(())
+}
+
+fn write_impl(w: &mut dyn Write, choice: &Choice) -> Result<(), std::io::Error> {
+    writeln!(w, "impl {} {{", choice.id.name.to_upper_camel_case())?;
+    writeln!(w)?;
+    indent(w, |w| {
+        write_serializer(w, choice)?;
+        writeln!(w)?;
+        write_deserializer(w, choice)?;
+        Ok(())
+    })?;
+    writeln!(w, "}}")?;
+    Ok(())
+}
+
+fn write_serializer(w: &mut dyn Write, choice: &Choice) -> Result<(), std::io::Error> {
+    writeln!(w, "pub(crate) fn write<W>(&self, writer: &mut EventWriter<W>, name: &str) -> core::result::Result<(), xml::writer::Error> where W: std::io::Write {{")?;
+    indent(w, |w| {
+        writeln!(w, "writer.write(events::XmlEvent::start_element(name))?;")?;
+        writeln!(w, "match self {{")?;
+        indent(w, |w| {
+            for var in choice.variants.iter() {
+                writeln!(
+                    w,
+                    "{}::{}(x) => {{",
+                    choice.id.name.to_upper_camel_case(),
+                    var.element_name.to_upper_camel_case()
+                )?;
+                indent(w, |w| {
+                    let tx = var.type_info.write_transform("x", &var.element_name);
+                    writeln!(w, "{}", tx)?;
+                    Ok(())
+                })?;
+                writeln!(w, "}}")?;
+            }
+            Ok(())
+        })?;
+        writeln!(w, "}}")?;
+        writeln!(w, "writer.write(events::XmlEvent::end_element())?;")?;
+        writeln!(w, "Ok(())")?;
+        Ok(())
+    })?;
+    writeln!(w, "}}")?;
+    Ok(())
+}
+
+fn write_deserializer(_w: &mut dyn Write, _choice: &Choice) -> Result<(), std::io::Error> {
     Ok(())
 }
