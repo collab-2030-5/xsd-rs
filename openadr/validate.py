a013537f4b5646621b4e5459ab881f5fe5b6e863
benjamin <ben@nebland.com>
2023-08-23 16:42:01 -0500

    Only print errors in validate.py

--- openadr/validate.py
+++ openadr/validate.py
@@ -13,9 +13,7 @@ file = open('openadr/xsd/pristine/oadr_20b.xsd').read()
 schema = xmlschema.XMLSchema(file, base_url='openadr/xsd/pristine/')
 
 for filename in glob.iglob(path + '**/*.xml', recursive=True):
-    print("Validating file", filename)
-    
     try:
         schema.validate(filename)
     except Exception as err:
-        print(f"  Unexpected {err=}, \n  {type(err)=}")
+        print(f"  Error validating file {filename}: {err=}")
